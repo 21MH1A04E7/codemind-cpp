fe59b67ddb77284edcc1334522b8026849b68ce3
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2022-12-15 20:42:53 +0530

    #include<iostream> using namespace std; int main(){     int n;     cin>>n;     int *arr=new int[n];     for(int i=0;i<n;i++){         cin>>arr[i];     }     int c=0;     for(int i=0;i+2<n;i++){         if(arr[i]%2==0&&arr[i+2]%2==0){             if(arr[i+1]%2!=0){                 c++;             }         }     }     cout<<c;     delete arr; }

---
+++ count_odd_numbers_between_even_numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int c=0;
+    for(int i=0;i+2<n;i++){
+        if(arr[i]%2==0&&arr[i+2]%2==0){
+            if(arr[i+1]%2!=0){
+                c++;
+            }
+        }
+    }
+    cout<<c;
+    delete arr;
+}
