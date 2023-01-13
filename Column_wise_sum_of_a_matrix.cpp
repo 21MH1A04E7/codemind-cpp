1bcf4e4fcbd23eca306b53f2ee78e100515cff02
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-01-14 02:09:09 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main(){     int n,m;     cin>>n>>m;     int arr[n][m];     for(int i=0;i<n;i++){         for(int j=0;j<m;j++){             cin>>arr[i][j];         }     }     for(int i=0;i<m;i++){         int sum_c=0;         for(int j=0;j<n;j++){             sum_c+=arr[j][i];         }         cout<<sum_c<<" ";     } }

---
+++ Column_wise_sum_of_a_matrix.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<m;i++){
+        int sum_c=0;
+        for(int j=0;j<n;j++){
+            sum_c+=arr[j][i];
+        }
+        cout<<sum_c<<" ";
+    }
+}
