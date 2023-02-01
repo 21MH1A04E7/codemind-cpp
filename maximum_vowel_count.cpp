28844ff12457748f78267b5cc8e1758adf23f677
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-02-01 21:37:13 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; bool fun(char s){     if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||     s=='A'||s=='E'||s=='I'||s=='O'||s=='U'){         return true;     }else{         return false;     } } int main(){     string str;     getline(cin,str);     int ans=0,count=0;     for(int i=0;i<str.length();i++){         if(fun(str[i]))         count++;         if(str[i]==' '){             ans=max(ans,count);             count=0;         }     }     ans=max(ans,count);     cout<<ans<<endl;
    }

---
+++ maximum_vowel_count.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool fun(char s){
+    if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||
+    s=='A'||s=='E'||s=='I'||s=='O'||s=='U'){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main(){
+    string str;
+    getline(cin,str);
+    int ans=0,count=0;
+    for(int i=0;i<str.length();i++){
+        if(fun(str[i]))
+        count++;
+        if(str[i]==' '){
+            ans=max(ans,count);
+            count=0;
+        }
+    }
+    ans=max(ans,count);
+    cout<<ans<<endl;
+    
+}
