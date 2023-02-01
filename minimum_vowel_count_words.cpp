9c50859d61e09a420f17874de83e834575ea1d21
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-02-01 21:46:23 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; bool fun(char s){     if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||     s=='A'||s=='E'||s=='I'||s=='O'||s=='U'){         return true;     }else{         return false;     } } int main(){     string str;     getline(cin,str);     int ans=INT_MAX,count=0;     for(int i=0;i<str.length();i++){         if(fun(str[i]))         count++;         if(str[i]==' '){             ans=min(ans,count);             count=0;         }     }     ans=min(ans,count);     int c=0,finalans=0;     for(int i=0;i<str.length();i++){         if(fun(str[i]))         c++;         if(str[i]==' '){             if(c==ans)             finalans++;             c=0;         }     }     if(c==ans)     finalans++;     cout<<finalans<<endl; }

---
+++ minimum_vowel_count_words.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool fun(char s){
+    if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u'||
+    s=='A'||s=='E'||s=='I'||s=='O'||s=='U'){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main(){
+    string str;
+    getline(cin,str);
+    int ans=INT_MAX,count=0;
+    for(int i=0;i<str.length();i++){
+        if(fun(str[i]))
+        count++;
+        if(str[i]==' '){
+            ans=min(ans,count);
+            count=0;
+        }
+    }
+    ans=min(ans,count);
+    int c=0,finalans=0;
+    for(int i=0;i<str.length();i++){
+        if(fun(str[i]))
+        c++;
+        if(str[i]==' '){
+            if(c==ans)
+            finalans++;
+            c=0;
+        }
+    }
+    if(c==ans)
+    finalans++;
+    cout<<finalans<<endl;
+}
