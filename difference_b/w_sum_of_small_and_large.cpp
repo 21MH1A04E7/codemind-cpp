665535d8e629692069ff5302c97359e585e55f8b
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-02-03 21:39:21 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main(){     string str;     getline(cin,str);     string word="";     int small=0,large=0;     for(int i=0;i<str.length();i++){         if(str[i]==' '){             char s='z';             char l='A';             for(int i=0;i<word.length();i++){                 s=min(s,word[i]);                 l=max(l,word[i]);             }             small+=int(s);             large+=int(l);             word="";         }else{              word+=str[i];         }
        }
               char s='z';
                char l='A';
                for(int i=0;i<word.length();i++){
                    s=min(s,word[i]);
                    l=max(l,word[i]);
                }
                small+=int(s);
                large+=int(l);
                cout<<abs(small-large)<<endl;
                
    }

---
+++ difference_b/w_sum_of_small_and_large.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+    string word="";
+    int small=0,large=0;
+    for(int i=0;i<str.length();i++){
+        if(str[i]==' '){
+            char s='z';
+            char l='A';
+            for(int i=0;i<word.length();i++){
+                s=min(s,word[i]);
+                l=max(l,word[i]);
+            }
+            small+=int(s);
+            large+=int(l);
+            word="";
+        }else{
+             word+=str[i];
+        }
+       
+    }
+           char s='z';
+            char l='A';
+            for(int i=0;i<word.length();i++){
+                s=min(s,word[i]);
+                l=max(l,word[i]);
+            }
+            small+=int(s);
+            large+=int(l);
+            cout<<abs(small-large)<<endl;
+            
+}
