7c9ebc519a23b51b0acd006ebc86f20305c869b0
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-08-18 01:46:14 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main(){     string str;     getline(cin,str);     set<char>st;     for(int i=0;i<str.length();i++){         if(str[i]==' '){             continue;         }else{             st.insert(tolower(str[i]));         }     }     for(auto it:st){         cout<<it;     } }

--- Distinct_characters.cpp
+++ Distinct_characters.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     string str;
     getline(cin,str);
-    map<char,int>mpp;
+    set<char>st;
     for(int i=0;i<str.length();i++){
         if(str[i]==' '){
             continue;
         }else{
-            mpp[tolower(str[i])]++;
+            st.insert(tolower(str[i]));
         }
     }
-    for(auto it:mpp){
-       cout<<it.first;
+    for(auto it:st){
+        cout<<it;
     }
 }
