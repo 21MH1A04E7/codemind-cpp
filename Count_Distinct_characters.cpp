f7b43efb14c460111e85a775bb50172c01062025
21MH1A04E7 <97781788+21MH1A04E7@users.noreply.github.com>
2023-08-18 01:54:03 +0530

    #include<iostream> #include<bits/stdc++.h> using namespace std; int main(){     string str;     getline(cin,str);     set<char>st;     for(int i=0;i<str.length();i++){         if(str[i]==' '){             continue;         }else{             st.insert(tolower(str[i]));         }     }     cout<<st.size()<<endl; }

--- Count_Distinct_characters.cpp
+++ Count_Distinct_characters.cpp
@@ -1,24 +1,16 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     string str;
     getline(cin,str);
-    unordered_map<char,int>mpp;
+    set<char>st;
     for(int i=0;i<str.length();i++){
         if(str[i]==' '){
             continue;
         }else{
-            mpp[tolower(str[i])]++;
+            st.insert(tolower(str[i]));
         }
     }
-    cout<<mpp.size()<<endl;
+    cout<<st.size()<<endl;
 }
